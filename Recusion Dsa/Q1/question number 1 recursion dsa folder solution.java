26b0ee06559359528bd1038435c5400f801227d4
Himanshu Singh <himanshusiingh3009@gmail.com>
2026-02-06 23:54:42 +0530

    question number 1 recursion dsa solution.java

--- Recusion Dsa/Q1/question number 1 recursion dsa folder solution.java
+++ Recusion Dsa/Q1/question number 1 recursion dsa folder solution.java
@@ -5,22 +5,22 @@ class Solution {
         int sign = 1;
         long num = 0;
 
-        // 1️⃣ Skip leading whitespaces
+        //Skip leading whitespaces
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
 
-        // 2️⃣ Handle sign
+        //Handle sign
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
 
-        // 3️⃣ Convert digits
+        //Convert digits
         while (i < n && Character.isDigit(s.charAt(i))) {
             num = num * 10 + (s.charAt(i) - '0');
 
-            // 4️⃣ Clamp on overflow
+            //Clamp on overflow
             if (sign * num >= Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
             }
